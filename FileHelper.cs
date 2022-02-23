fa80ffbe1a944af22d8b4dbce337ae4241a544ed
Mike Knights <mk@mkdev.co.uk>
2022-02-23 14:42:28 +0000

    mk.helpers.1.1.5

---
+++ FileHelper.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mk.helpers
+{
+
+    public static class FileHelper
+    {
+        public class DownloadFileResult
+        {
+            public string FilePath { get; set; }
+            public long Size { get; set; }
+        }
+
+        public static async Task<DownloadFileResult> DownloadFileAsync(string url, Action<long>? onDownloadProgress, string filePath = null)
+        {
+            var client = new HttpClient();
+
+            filePath = filePath ?? Path.GetTempFileName();
+            if (!(filePath.Contains("/") || filePath.Contains("\\")))
+                filePath = $"{Path.GetTempPath()}{filePath}";
+
+            File.Delete(filePath + ".incomplete");
+            File.Delete(filePath);
+
+            long totalSize = 0;
+            long temp = 0;
+            using (var file = new FileStream(filePath + ".incomplete", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                await client.DownloadDataAsync(url, file, (progress) =>
+                {
+                    totalSize = progress;
+                    if (progress > 1024 || temp < progress - 1024)
+                    {
+                        onDownloadProgress?.Invoke(totalSize);
+                        temp = progress;
+                    }
+                });
+                file.Close();
+            }
+            Thread.Sleep(500);// Might be locked;
+            File.Move(filePath + ".incomplete", filePath);
+
+            return new DownloadFileResult
+            {
+                FilePath = filePath,
+                Size = totalSize,
+            };
+        }
+
+        public static void ReadJsonArrayFile<T>(string filePath, Action<T> onData, Action<double> onProgress, bool readSynchronously = false)
+        {
+            Task[] tasks = new Task[10];
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+            JsonSerializer serializer = new JsonSerializer();
+
+            using FileStream s = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using StreamReader sr = new StreamReader(s);
+            using JsonReader reader = new JsonTextReader(sr);
+            var lastProgress = DateTime.Now;
+
+            while (reader.Read())
+            {
+                // deserialize only when there's "{" character in the stream
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    var o = serializer.Deserialize<T>(reader);
+
+
+                    if (readSynchronously)
+                    {
+                        onData?.Invoke(o);
+                    }
+                    else
+                    {
+                        while (!tasks.Any(d => d == null || d.IsCompletedSuccessfully))
+                        {
+                            Thread.Sleep(1);
+                        }
+
+                        var indic = tasks.Select((s, i) => new { s, i })
+                            .Where(d => d.s == null || d.s.IsCompletedSuccessfully)
+                            .Select(d => d.i).First();
+
+                        tasks[indic] = Task.Factory.StartNew(() =>
+                        {
+                            onData?.Invoke(o);
+                        });
+                    }
+
+                    if (lastProgress.AddSeconds(2) < DateTime.Now)
+                    {
+                        lastProgress = DateTime.Now;
+                        var progress = Math.Round((double)sr.BaseStream.Position / sr.BaseStream.Length * 100, 2);
+                        onProgress?.Invoke(progress);
+                    }
+
+                }
+            }
+            return;
+        }
+    }
+
+
+}
--- HttpClientExtensions.cs
+++ HttpClientExtensions.cs
@@ -1,51 +1,185 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace mk.helpers
 {
-	public static class HttpClientExtensions
-	{
-		public static async Task DownloadFileWithProgressAsync(this HttpClient client, string requestUrl, Stream destination, Action<long> downloadProgress = null, CancellationToken cancellationToken = default(CancellationToken))
-		{
-			using (var response = await client.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead))
-			{
-				var contentLength = response.Content.Headers.ContentLength;
-				using (var download = await response.Content.ReadAsStreamAsync())
-				{
+    public static class HttpClientExtensions
+    {
 
-					if (download is null)
-						throw new ArgumentNullException(nameof(download));
-					if (!download.CanRead)
-						throw new InvalidOperationException($"'{nameof(download)}' is not readable.");
-					if (destination == null)
-						throw new ArgumentNullException(nameof(destination));
-					if (!destination.CanWrite)
-						throw new InvalidOperationException($"'{nameof(destination)}' is not writable.");
+        public static async Task DownloadDataAsync(this HttpClient client, string requestUrl, Stream destination, Action<long> downloadProgress = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            using (var response = await client.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead))
+            {
+                var contentLength = response.Content.Headers.ContentLength;
+                using (var download = await response.Content.ReadAsStreamAsync())
+                {
 
-					var buffer = new byte[10000];
-					long totalBytesRead = 0;
-					int bytesRead;
-					DateTime lastUpdate = DateTime.Now;
-					while ((bytesRead = await download.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
-					{
-						await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
-						totalBytesRead += bytesRead;
-						if (lastUpdate.AddSeconds(1) < DateTime.Now)
-						{
-							downloadProgress?.Invoke(totalBytesRead);
-							lastUpdate = DateTime.Now;
-						}
-					}
+                    if (download is null)
+                        throw new ArgumentNullException(nameof(download));
+                    if (!download.CanRead)
+                        throw new InvalidOperationException($"'{nameof(download)}' is not readable.");
+                    if (destination == null)
+                        throw new ArgumentNullException(nameof(destination));
+                    if (!destination.CanWrite)
+                        throw new InvalidOperationException($"'{nameof(destination)}' is not writable.");
 
-					downloadProgress?.Invoke(totalBytesRead);
-				}
-			}
-		}
+                    var buffer = new byte[10000];
+                    long totalBytesRead = 0;
+                    int bytesRead;
+                    DateTime lastUpdate = DateTime.Now;
+                    while ((bytesRead = await download.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
+                    {
+                        await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+                        totalBytesRead += bytesRead;
+                        if (lastUpdate.AddSeconds(1) < DateTime.Now)
+                        {
+                            downloadProgress?.Invoke(totalBytesRead);
+                            lastUpdate = DateTime.Now;
+                        }
+                    }
 
-	}
+                    downloadProgress?.Invoke(totalBytesRead);
+                }
+            }
+        }
+
+
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonConvert.SerializeObject(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PostAsync(url, content);
+        }
+        public static Task<HttpResponseMessage> PutAsJsonAsync<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonConvert.SerializeObject(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PutAsync(url, content);
+        }
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+        {
+            var dataAsString = await content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonConvert.DeserializeObject<T>(dataAsString);
+        }
+        public static Task<HttpResponseMessage> HeadAsync(this HttpClient client, string requestUri)
+        {
+            return client.HeadAsync(new Uri(requestUri));
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(this HttpClient client, Uri requestUri)
+        {
+            return client.HeadAsync(requestUri, HttpCompletionOption.ResponseContentRead);
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(
+            this HttpClient client,
+            string requestUri,
+            HttpCompletionOption completionOption
+        )
+        {
+            return client.HeadAsync(new Uri(requestUri), completionOption);
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(
+            this HttpClient client,
+            Uri requestUri,
+            HttpCompletionOption completionOption
+        )
+        {
+            return client.HeadAsync(requestUri, completionOption, CancellationToken.None);
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(
+            this HttpClient client,
+            string requestUri,
+            CancellationToken cancellationToken
+        )
+        {
+            return client.HeadAsync(new Uri(requestUri), cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(
+            this HttpClient client,
+            Uri requestUri,
+            CancellationToken cancellationToken
+        )
+        {
+            return client.HeadAsync(requestUri, HttpCompletionOption.ResponseContentRead, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(
+            this HttpClient client,
+            string requestUri,
+            HttpCompletionOption completionOption,
+            CancellationToken cancellationToken
+        )
+        {
+            return client.HeadAsync(new Uri(requestUri), completionOption, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> HeadAsync(
+            this HttpClient client,
+            Uri requestUri,
+            HttpCompletionOption completionOption,
+            CancellationToken cancellationToken
+        )
+        {
+            return client.SendAsync(new HttpRequestMessage(HttpMethod.Head, requestUri), completionOption,
+                cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> SendAsync(
+            this HttpClient client,
+            HttpMethod method,
+            Uri requestUri,
+            IEnumerable<KeyValuePair<string, IEnumerable<string>>> headers = null,
+            HttpContent content = null
+        )
+        {
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = content
+            };
+
+            if (!(headers is null))
+            {
+                foreach (var (key, value) in headers)
+                {
+                    request.Headers.Add(key, value);
+                }
+            }
+
+            return client.SendAsync(request);
+        }
+
+        public static async Task<HttpResponseMessage> SendRequestWithBearerTokenAsync(this HttpClient httpClient, HttpMethod method, string path, object requestBody, string accessToken, CancellationToken ct)
+        {
+            var request = new HttpRequestMessage(method, path);
+
+            if (requestBody != null)
+            {
+                var json = JsonConvert.SerializeObject(requestBody, Formatting.None);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                request.Content = content;
+            }
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.SendAsync(request, ct);
+            return response;
+        }
+    }
 }
